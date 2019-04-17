09259f92363bdeea63c636b65738911cc7f52132
Alberto Resco <alperez@pivotal.io>
2019-04-17 15:11:30 +0800

    Add UI failing test

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -1,5 +1,26 @@
-describe("foo", function () {
-    it("bars", function () {
-        expect(true).toBe(false)
-    })
+import React from 'react';
+import * as ReactDOM from "react-dom";
+import {RPSApp} from "../src/RPSApp";
+
+describe("play form", function () {
+    describe('when the play use case tells the UI that the input is invalid', () => {
+        it('tells the user that their input is invalid', () => {
+            let domFixture = document.createElement('div');
+
+            document.body.appendChild(domFixture);
+
+            let alwaysInvalidRequest = {
+                play: (p1, p2, observer) => observer.invalid()
+            };
+
+            ReactDOM.render(
+                <RPSApp request={alwaysInvalidRequest}/>,
+                domFixture
+            );
+
+            document.querySelector('button').click();
+            expect(domFixture.innerText).toContain('INVALID!')
+        });
+
+    });
 })
---
+++ web/src/RPSApp.js
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+export class RPSApp extends React.Component {
+
+    render() {
+        return '';
+    }
+}
