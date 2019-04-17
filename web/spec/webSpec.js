a19b48e933d94a66056870bdc39be3149027e11d
Alberto Resco <alperez@pivotal.io>
2019-04-17 16:30:38 +0800

    Add all scenarios

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -2,25 +2,97 @@ import React from 'react';
 import * as ReactDOM from "react-dom";
 import {RPSApp} from "../src/RPSApp";
 
-describe("play form", function () {
-    describe('when the play use case tells the UI that the input is invalid', () => {
-        it('tells the user that their input is invalid', () => {
-            let domFixture = document.createElement('div');
+describe("play form", () => {
+    let domFixture;
 
-            document.body.appendChild(domFixture);
+    beforeEach(() => {
+        domFixture = document.createElement('div');
+        domFixture.id = 'reactApp';
 
-            let alwaysInvalidRequest = {
+        document.body.appendChild(domFixture);
+    });
+
+    afterEach(() => {
+        domFixture.remove();
+    });
+
+    describe('the game logic reported that the input is invalid', () => {
+        beforeEach(() => {
+            renderApp({
                 play: (p1, p2, observer) => observer.invalid()
-            };
+            });
+        });
+        it('tells the user that their input is invalid', () => {
+            expect(page()).not.toContain('INVALID!')
 
-            ReactDOM.render(
-                <RPSApp request={alwaysInvalidRequest}/>,
-                domFixture
-            );
+            submitForm();
 
-            document.querySelector('button').click();
-            expect(domFixture.innerText).toContain('INVALID!')
+            expect(page()).toContain('INVALID!')
         });
 
     });
-})
+
+    describe("the game logic reported that P1 won", function () {
+        beforeEach(function () {
+            renderApp({
+                play: function (p1, p2, observer) {
+                    observer.p1Wins()
+                }
+            });
+        });
+
+        it("displays 'P1 Wins!'", function () {
+            expect(page()).not.toContain("P1 Wins!")
+            submitForm()
+            expect(page()).toContain("P1 Wins!")
+        })
+    });
+
+    describe("the game logic reported that P2 won", function () {
+        beforeEach(function () {
+            renderApp({
+                play: function (p1, p2, observer) {
+                    observer.p2Wins()
+                }
+            });
+        });
+
+
+        it("displays 'P2 Wins!'", function () {
+            expect(page()).not.toContain("P2 Wins!")
+            submitForm()
+            expect(page()).toContain("P2 Wins!")
+        })
+    });
+
+    describe("the game logic reported that it was a draw", function () {
+        beforeEach(function () {
+            renderApp({
+                play: function (p1, p2, observer) {
+                    observer.draw()
+                }
+            });
+        });
+
+        it("displays 'DRAW'", function () {
+            expect(page()).not.toContain("DRAW")
+            submitForm()
+            expect(page()).toContain("DRAW")
+        });
+    });
+
+    function renderApp(useCases) {
+        ReactDOM.render(
+            <RPSApp requests={useCases}/>,
+            domFixture
+        );
+    }
+
+    function submitForm() {
+        document.querySelector('button').click();
+    }
+
+    function page() {
+        return domFixture.innerText;
+    }
+});
--- web/src/RPSApp.js
+++ web/src/RPSApp.js
@@ -1,8 +1,36 @@
 import * as React from "react";
 
 export class RPSApp extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    submitHandler() {
+        this.props.requests.play("p1 throw placeholder", "p2 throw placeholder", this)
+    }
+
+    invalid(){
+        this.setState({result: "INVALID!"})
+    }
+
+    draw(){
+        this.setState({result: "DRAW"})
+    }
+
+    p1Wins(){
+        this.setState({result: "P1 Wins!"})
+    }
+
+    p2Wins(){
+        this.setState({result: "P2 Wins!"})
+    }
 
     render() {
-        return '';
+        return <div>
+            {this.state.result}
+            <button onClick={this.submitHandler.bind(this)}>PLAY</button>
+        </div>
+
     }
 }
